eb2346a6fb9626cc0df450de157d36d0d8943efd
Jian Yeung <yeungjian@gmail.com>
2019-07-22 11:28:49 -0700

    finished bee sprint

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
-};
+	Grub.call(this);
 
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
+};
+//this.prototype.constructor = Grub;
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
